6d21ee82b7723121355b998a6940f96b9431faab
Ali Husnain <alihusnaintcf970@gmail.com>
2025-07-30 14:26:46 +0500

    Nothing

--- ERPsystem1/obj/Debug/net8.0/ERPsystem1.AssemblyInfo.cs
+++ ERPsystem1/obj/Debug/net8.0/ERPsystem1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPsystem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1ff7949e0e816651c5ca583161f85bada7052c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPsystem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPsystem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
